4037cbd9eee22d94951a71af49490a8fbc977895
Dan langille <dan@langille.org>
2020-12-23 19:22:30 +0000

    set filesystems for packages and ports cache

--- 18-post-jail-creation-configuration-ingress.sh
+++ 18-post-jail-creation-configuration-ingress.sh
@@ -4,10 +4,11 @@
 
 jexec ingress01 zfs set canmount=on ${freebsdzpool}/freshports/ingress01/cache/html
 
-jexec ingress01 zfs set mountpoint=/var/db/freshports/cache ${freebsdzpool}/freshports/ingress01/cache
-jexec ingress01 zfs inherit mountpoint ${freebsdzpool}/freshports/ingress01/cache/html
+jexec ingress01 zfs set mountpoint=/var/db/freshports/cache/packages ${freebsdzpool}/freshports/ingress01/cache/packages
+jexec ingress01 zfs set mountpoint=/var/db/freshports/cache/ports    ${freebsdzpool}/freshports/ingress01/cache/ports
+#jexec ingress01 zfs inherit mountpoint ${freebsdzpool}/freshports/ingress01/cache/html
 
-jexec ingress01 zfs mount ${freebsdzpool}/freshports/ingress01/cache/html
+#jexec ingress01 zfs mount ${freebsdzpool}/freshports/ingress01/cache/html
 
 # we need www on the ingress side so we have the correct permissions on the webserver
 jexec ingress01 chown -R www:freshports /var/db/freshports/cache
