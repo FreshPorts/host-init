dc8178cd5338d17eccbeef9b67e0e91199eb2d21
Dan langille <dan@langille.org>
2020-12-23 19:18:08 +0000

    add mx-ingress04

--- 03-create-jails.sh
+++ 03-create-jails.sh
@@ -9,3 +9,10 @@ echo creating ingress01
 
 echo creating nginx01
 ./src/bin/mkjail create -v 12.1-RELEASE -j nginx01 -f nginx
+
+#
+# 04 here, because we mx-ingress hostnames are public and we already
+# have both 01 and 02 and 03.
+#
+echo creating mx-ingress04
+./src/bin/mkjail create -v 12.1-RELEASE -j mx-ingress04 -f default
