c906d84e32e7f646074026383e236127a06347f1
Dan Langille <dan@langille.org>
2020-12-29 01:22:35 +0000

    fix nginx cert name, it is aws-1, not aws-1-nginx.  nginx needs both postfix and nginx restart on new cert. JAILS var is now on jails-vars.sh

--- 06-install-local-files.sh
+++ 06-install-local-files.sh
@@ -7,7 +7,6 @@
 
 # which jails have certs and need cert-puller configured
 
-JAILS="ingress01 nginx01 mx-ingress04"
 for jail in $JAILS
 do
   mkdir -p ${jailroot}/$jail/usr/local/etc/anvil
@@ -16,24 +15,28 @@ done
 
 # this is harder to put into a for loop because the jail name and the cert name do corresponds
 
-sed -i '' -e 's/%%MYCERTS%%/aws-1-ingress01.freshports.org/g' ${jailroot}/ingress01/usr/local/etc/anvil/cert-puller.conf
-sed -i '' -e 's/%%SERVICES_RESTART%%/SERVICES_RESTART="postfix"/g'               ${jailroot}/ingress01/usr/local/etc/anvil/cert-puller.conf
-sed -i '' -e 's/%%SERVICES_RELOAD%%//g'                       ${jailroot}/ingress01/usr/local/etc/anvil/cert-puller.conf
+sed -i '' -e 's/%%MYCERTS%%/aws-1-ingress01.freshports.org/g'           ${jailroot}/ingress01/usr/local/etc/anvil/cert-puller.conf
+sed -i '' -e 's/%%SERVICES_RESTART%%/SERVICES_RESTART="postfix"/g'      ${jailroot}/ingress01/usr/local/etc/anvil/cert-puller.conf
+sed -i '' -e 's/%%SERVICES_RELOAD%%//g'                                 ${jailroot}/ingress01/usr/local/etc/anvil/cert-puller.conf
 
-sed -i '' -e 's/%%MYCERTS%%/aws-1-nginx01.freshports.org/g'   ${jailroot}/nginx01/usr/local/etc/anvil/cert-puller.conf
-sed -i '' -e 's/%%SERVICES_RESTART%%//g'                      ${jailroot}/nginx01/usr/local/etc/anvil/cert-puller.conf
-sed -i '' -e 's/%%SERVICES_RELOAD%%/SERVICES_RELOAD="nginx"/g'                  ${jailroot}/nginx01/usr/local/etc/anvil/cert-puller.conf
+sed -i '' -e 's/%%MYCERTS%%/aws-1.freshports.org/g'                     ${jailroot}/nginx01/usr/local/etc/anvil/cert-puller.conf
+sed -i '' -e 's/%%SERVICES_RESTART%%//g'                                ${jailroot}/nginx01/usr/local/etc/anvil/cert-puller.conf
+sed -i '' -e 's/%%SERVICES_RELOAD%%/SERVICES_RELOAD="nginx postfix"/g'  ${jailroot}/nginx01/usr/local/etc/anvil/cert-puller.conf
 
-sed -i '' -e 's/%%MYCERTS%%/mx-ingress04.freshports.org/g'    ${jailroot}/mx-ingress04/usr/local/etc/anvil/cert-puller.conf
-sed -i '' -e 's/%%SERVICES_RESTART%%/SERVICES_RELOAD="postfix"/g'               ${jailroot}/mx-ingress04/usr/local/etc/anvil/cert-puller.conf
-sed -i '' -e 's/%%SERVICES_RELOAD%%//g'                       ${jailroot}/mx-ingress04/usr/local/etc/anvil/cert-puller.conf
+sed -i '' -e 's/%%MYCERTS%%/mx-ingress04.freshports.org/g'              ${jailroot}/mx-ingress04/usr/local/etc/anvil/cert-puller.conf
+sed -i '' -e 's/%%SERVICES_RESTART%%/SERVICES_RELOAD="postfix"/g'       ${jailroot}/mx-ingress04/usr/local/etc/anvil/cert-puller.conf
+sed -i '' -e 's/%%SERVICES_RELOAD%%//g'                                 ${jailroot}/mx-ingress04/usr/local/etc/anvil/cert-puller.conf
 
 # now set the sudo permissions for each jail
 
-JAILS="ingress01 nginx01 mx-ingress04"
 for jail in $JAILS
 do
   # we have to create it because sudo has not yet been installed
   mkdir -p ${jailroot}/$jail/usr/local/etc/sudoers.d/
-  sudo jexec ingress01 /usr/local/bin/cert-puller -s > ${jailroot}/$jail/usr/local/etc/sudoers.d/anvil
+
+  # set sudo permissions for anvil re cert-puller
+  sudo jexec $jail /usr/local/bin/cert-puller -s > ${jailroot}/$jail/usr/local/etc/sudoers.d/anvil
+
+  # pull down the certs
+  sudo jexec -U anvil $jail /usr/local/bin/cert-puller
 done
