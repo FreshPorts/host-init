b596e9154deb8ab0d7a826e6d55c161abaf8979a
Dan Langille <dan@langille.org>
2021-02-19 21:07:10 +0000

    change /var/db/freshports/var/db/freshports/cache to /var/db/freshports/cache

--- 07-post-jail-creation-configuration-nginx.sh
+++ 07-post-jail-creation-configuration-nginx.sh
@@ -2,7 +2,7 @@
 
 . /usr/local/etc/host-init/jail-vars.sh
 
-zfs set jailed=on ${datazpool}/freshports/$WEB_JAIL/var/db/freshports/cache
+zfs set jailed=on ${datazpool}/freshports/$WEB_JAIL/var/db/freshports
 
 jexec $WEB_JAIL zfs set mountpoint=/var/db/freshports/cache ${datazpool}/freshports/$WEB_JAIL/var/db/freshports/cache
 
@@ -14,7 +14,7 @@ do
    # allow freshports to rollback
    jexec $WEB_JAIL zfs allow freshports rollback ${datazpool}/freshports/$WEB_JAIL/var/db/freshports/cache/$set
    jexec $WEB_JAIL zfs mount                     ${datazpool}/freshports/$WEB_JAIL/var/db/freshports/cache/$set
-   jexec $WEB_JAIL chown www:freshports /var/db/freshports/var/db/freshports/cache/$set
+   jexec $WEB_JAIL chown www:freshports /var/db/freshports/cache/$set
 
    # this snapshot must be last or you'll undo the chown above when you rollback
    jexec $WEB_JAIL zfs snapshot                  ${datazpool}/freshports/$WEB_JAIL/var/db/freshports/cache/$set@empty
