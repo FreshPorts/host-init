0c7573cacdeffd82c0e48910cad8f1cadd48d86f
Dan Langille <dan@langille.org>
2021-09-08 17:06:02 +0000

    include searchlog.txt

--- 07-post-jail-creation-configuration-nginx.sh
+++ 07-post-jail-creation-configuration-nginx.sh
@@ -22,3 +22,24 @@ do
    jexec $WEB_JAIL zfs snapshot                  ${datazpool}/freshports/$WEB_JAIL/var/db/freshports/cache/$set@empty
 done
 
+# Enable the search log. This has always been a thing, and never really used.
+jexec $WEB_JAIL touch                /var/db/freshports/cache/searchlog.txt
+jexec $WEB_JAIL chown www:freshports /var/db/freshports/cache/searchlog.txt
+jexec $WEB_JAIL chmod 0644           /var/db/freshports/cache/searchlog.txt
+
+# I am not sure why this needs to be done, but when configuring x8dtu, it was required.
+#zfs inherit mountpoint main_tank/freshports/$INGRESS_JAIL/var/db/freshports/cache/html 
+#zfs inherit mountpoint main_tank/freshports/$INGRESS_JAIL/var/db/freshports/cache/spooling
+#zfs inherit mountpoint main_tank/freshports/$INGRESS_JAIL/var/db/freshports/message-queues
+#zfs inherit mountpoint main_tank/freshports/$INGRESS_JAIL/var/db/freshports/repos
+#zfs inherit mountpoint main_tank/freshports/$INGRESS_JAIL/var/db/ingress/message-queues
+#zfs inherit mountpoint main_tank/freshports/$INGRESS_JAIL/var/db/ingress/repos
+#
+#zfs mount main_tank/freshports/$INGRESS_JAIL/var/db/freshports/cache/html 
+#zfs mount main_tank/freshports/$INGRESS_JAIL/var/db/freshports/cache/spooling
+#zfs mount main_tank/freshports/$INGRESS_JAIL/var/db/freshports/message-queues
+#zfs mount main_tank/freshports/$INGRESS_JAIL/var/db/freshports/repos
+#zfs mount main_tank/freshports/$INGRESS_JAIL/var/db/ingress/message-queues
+#zfs mount main_tank/freshports/$INGRESS_JAIL/var/db/ingress/repos
+
+jexec $WEB_JAIL mkdir                /var/db/freshports/cache/html
