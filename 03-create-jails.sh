1317619b194967e920fb16c1e4975c96bd3f2d75
Dan Langille <dan@langille.org>
2021-09-08 16:59:59 +0000

    mkjail is now a port, no need to use mkjail_path. no more mx-jail

--- 03-create-jails.sh
+++ 03-create-jails.sh
@@ -2,24 +2,17 @@
 
 . /usr/local/etc/host-init/jail-vars.sh
 
-cd $mkjail_path
-
 if [ ! -z ${INGRESS_JAIL} ]; then
   echo creating ${INGRESS_JAIL}
-  ./src/bin/mkjail create -v ${VERSION}-RELEASE -j ${INGRESS_JAIL} -f ingress
+  mkjail create -v ${VERSION}-RELEASE -j ${INGRESS_JAIL} -f ingress
 fi
 
 if [ ! -z ${WEB_JAIL} ]; then
   echo creating ${WEB_JAIL}
-  ./src/bin/mkjail create -v ${VERSION}-RELEASE -j ${WEB_JAIL} -f nginx
-fi
-
-if [ ! -z ${MX_JAIL} ]; then
-  echo creating ${MX_JAIL}
-  ./src/bin/mkjail create -v ${VERSION}-RELEASE -j mx-ingress04 -f default
+  mkjail create -v ${VERSION}-RELEASE -j ${WEB_JAIL} -f nginx
 fi
 
 if [ ! -z ${PG_JAIL} ]; then
   echo creating ${PG_JAIL}
-  ./src/bin/mkjail create -v ${VERSION}-RELEASE -j ${PG_JAIL} -f default
+  mkjail create -v ${VERSION}-RELEASE -j ${PG_JAIL} -f default
 fi
